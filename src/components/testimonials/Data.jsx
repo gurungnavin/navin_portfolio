d9de6d8a51e018fc13ee787b727d267faa6f324f
gurungnavin <74055562+gurungnavin@users.noreply.github.com>
2023-07-04 01:26:13 +0900

    Your commit message here

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,35 @@
+import Image1 from "../../assets/testimonial1.png";
+import Image2 from "../../assets/testimonial2.png";
+import Image3 from "../../assets/testimonial3.png";
+import Image4 from "../../assets/testimonial4.png";
+
+export const Data = [
+  {
+    id: 1,
+    image: Image1,
+    title: "店長(seven-eleven)",
+    description:
+      "ナビンさんは頭が良く、仕事もすぐに憶える。どこの国でも稼ぐ力を持っていると思います。日本語も上手で、車の運転もできるし、エリートだと 店長は思っています。彼は教育もできるし、心の優しい温かい人だと思います。日本で学んだことを生かして、これからも頑張ってください。",
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: "伏嶋",
+    description:
+      "いつも仕事をテキパキとこなすことができるナビンさんには、いつもすごいなと感心していました。社会に出ても、これまでのナビンさんらしさを大切にして笑顔で頑張ってくださいね。長い間、お疲れ様でした。.",
+  },
+  {
+    id: 3,
+    image: Image3,
+    title: "田村",
+    description:
+      "シフトの変更など色々お世話になりました。新しい職場でもお身体ご自愛くださいませ。色々ありがとうございました。",
+  },
+  {
+    id: 4,
+    image: Image4,
+    title: "ハラダ",
+    description:
+      "ナビンさんとはいつも冗談ばかり言い合っていますたが、一緒に仕事をするのがとても楽しかったです。仕事もテキパキこなし、とても頼りになりました。今まで色々ありがとう。",
+  },
+];
---
+++ src/components/work/Data.jsx
@@ -0,0 +1,64 @@
+import Work1 from '../../assets/work1.jpg'
+import Work3 from '../../assets/work3.jpg'
+import Work4 from '../../assets/work4.jpg'
+import Work5 from '../../assets/work5.jpg'
+import Work6 from '../../assets/work6.jpg'
+import Work7 from '../../assets/work7.jpg'
+
+export const projectsData = [
+    {
+      id: 1,
+      image: Work1,
+      title: "'Susi' Restaurant",
+      category: "business websites",
+      link: 'https://gurungnavin.github.io/sushi-restaurant.github.io/',
+    },
+    {
+      id: 3,
+      image: Work3,
+      title: "Hiring (job hunting) ",
+      category: "websites",
+      link: 'https://gurungnavin.github.io/hiring.github.io/',
+    },
+    {
+      id: 4,
+      image: Work4,
+      title: "SocialBook",
+      category: "websites",
+      link: 'https://gurungnavin.github.io/300m.github.io/',
+    },
+    {
+      id: 5,
+      image: Work5,
+      title: "VidTube",
+      category: "websites",
+      link: 'https://gurungnavin.github.io/vidtube.github.io/',
+    },
+    {
+      id: 6,
+      image: Work6,
+      title: "CineFlix",
+      category: "websites",
+      link: 'https://gurungnavin.github.io/Cineflix.github.io/',
+    },
+    {
+      id: 7,
+      image: Work7,
+      title: "Portfolio Sample",
+      category: "websites",
+      link: 'https://gurungnavin.github.io/grgnvn.github.io/',
+    },
+  ];
+
+  export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'websites',
+    },
+
+    {
+      name: 'business websites'
+    }
+  ]
